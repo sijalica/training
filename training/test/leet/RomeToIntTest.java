c02ba58883c37be6db8d67c648cf90ceb91bbe65
sijalica <igorradivojevic996@gmail.com>
2023-04-05 15:29:28 +0200

    romeToInt

--- training/test/leet/RomeToIntTest.java
+++ training/test/leet/RomeToIntTest.java
@@ -1,4 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
-class RomeToIntTest {
-  
+package leet;
+
+import org.junit.jupiter.api.Test;
+
+import static leet.RomeToInt.romeToInt;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RomeToIntTest extends TwoSum {
+
+    @Test
+    void romeToIntTest() {
+        assertEquals(58, romeToInt("LVIII"));
+    }
 }
