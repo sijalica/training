01b37ac4c6bf9f0869d3d4ab770702fc0c7e6c2e
sijalica <igorradivojevic996@gmail.com>
2023-04-10 16:01:29 +0200

    builder

---
+++ effective-java/src/chapter1/item2builderpattern/Main.java
@@ -0,0 +1,10 @@
+package chapter1.item2builderpattern;
+
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+
+        System.out.println(cocaCola.getClass());
+    }
+}
